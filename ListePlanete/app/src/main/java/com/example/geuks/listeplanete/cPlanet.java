515522800eff8a815788d4586dc2e23f1871886f
geuks <G.Unalan@ludus-academie.com>
2018-12-07 09:36:36 +0100

    Ajout de commentaires

--- ListePlanete/app/src/main/java/com/example/geuks/listeplanete/cPlanet.java
+++ ListePlanete/app/src/main/java/com/example/geuks/listeplanete/cPlanet.java
@@ -5,12 +5,12 @@ import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+//Classe cPlanet qui permet de gérer les différents composants de l'affichage à travers leurs Get et Set
 public class cPlanet {
     private String name;
     private String text;
     private Bitmap avatar;
-
+   
     public void setName(String name){
         this.name = name;
     }
